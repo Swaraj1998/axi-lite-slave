942820310ca9faaeef26e533c4af4dce60d3f1d0
Swaraj Hota <swarajhota353@gmail.com>
2021-01-07 20:25:43 +0530

    auto create axi lite IP

--- sim.tcl
+++ sim.tcl
@@ -2,10 +2,18 @@ create_project -in_memory -part xc7z020clg400-1 -force axi_lite_slave_test
 set_property BOARD_PART em.avnet.com:microzed_7020:part0:1.1 [current_project]
 set_property default_lib work [current_project]
 
-create_bd_design "design_1"
+# Create axi lite IP
 
-set_property ip_repo_paths /media/hdd1/Xilinx/Projects/ip_repo [current_project]
+load_features ipservices
+exec mkdir -p axi_lite_ip
+exec cp axi_lite.vhd axi_lite_ip 
+ipx::infer_core -vendor user.org -library user -taxonomy /UserIP axi_lite_ip
+set_property ip_repo_paths axi_lite_ip [current_project]
 update_ip_catalog
+
+# Block design
+
+create_bd_design "design_1"
 
 create_bd_cell -type ip -vlnv user.org:user:axi_lite:1.0 axi_lite_0
 create_bd_cell -type ip -vlnv xilinx.com:ip:axi_vip:1.1 axi_vip_0
